ccb8d5053107edb219b5aea5bf7deef1ca80de26
Leon Garza <hugo.leongarza@bt.com>
2020-09-05 20:33:39 +0100

    created a rectangle as a surface and drawing it

--- GameLoop.py
+++ GameLoop.py
@@ -28,8 +28,9 @@ class GameLoop:
             print(config_data)
 
         else:
-
-            self.screen = pygame.display.set_mode((int(config_data['SCREEN_HEIGHT']), int(config_data['SCREEN_WIDTH'])))
+            config_data['SCREEN_HEIGHT'] = int(config_data['SCREEN_HEIGHT'])
+            config_data['SCREEN_WIDTH'] = int(config_data['SCREEN_WIDTH'])
+            self.screen = pygame.display.set_mode((config_data['SCREEN_WIDTH'], config_data['SCREEN_HEIGHT']))
         pygame.display.set_caption('CE889 Assignment Template')
         pygame.display.set_icon(pygame.image.load(config_data['LANDER_IMG_PATH']))
 
@@ -37,11 +38,14 @@ class GameLoop:
     def main_loop(self, config_data):
         # Creates the lander object
         lander = self.setup_lander(config_data)
-        surface = Surface(self.screen, (100,100), (200,200))
-        surface_sprites = pygame.sprite.Group()
+        # surface = Surface(self.screen, (100,100), (200,200))
+        print(config_data['SCREEN_WIDTH'])
+        surface = Surface((config_data['SCREEN_WIDTH'], config_data['SCREEN_HEIGHT']*.2), (0,50))
+        # surface_sprites = pygame.sprite.Group()
         sprites = pygame.sprite.Group()
         sprites.add(lander)
-        surface_sprites.add(surface)
+        sprites.add(surface)
+        # surface_sprites.add(surface)
 
 
         # The main loop of the window
@@ -52,10 +56,10 @@ class GameLoop:
             # painting white background
             self.screen.fill([255, 255, 255])
 
-            pygame.draw.line(self.screen, (255,0,0), (100,100), (200,200), 5)
+            # pygame.draw.line(self.screen, (255,0,0), (100,100), (200,200), 5)
             # then update the visuals on screen from the list
             sprites.draw(self.screen)
-            surface_sprites.draw(self.screen)
+            # surface_sprites.draw(self.screen)
             pygame.display.update()
             time.sleep(0.06)
 
--- Surface.py
+++ Surface.py
@@ -2,17 +2,20 @@ import pygame
 
 class Surface(pygame.sprite.Sprite):
 
-    def __init__(self, screen, start_point, end_point):
+    def __init__(self, screen_dimension, location):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface([200,5])
+        self.x = location[0]
+        self.y = location[1]
+
+        self.image = pygame.Surface([screen_dimension[0],screen_dimension[1]])
         self.image.fill((255,255,255))
         self.image.set_colorkey((255,255,255))
 
-        pygame.draw.rect(self.image, (255,0,0), [0,0,200,5])
+        # pygame.draw.rect(self.image, (255,0,0), [0,0,200,5])
 
-        self.rect = self.image.get_rect()
-
+        self.rect = pygame.draw.polygon(self.image, (0,0,0), [(0,0), (screen_dimension[0], 0), (screen_dimension[0], screen_dimension[1]), (0, screen_dimension[1])])        
+        
         # self.rect = pygame.draw.line(screen, (255,0,0), start_point, end_point, 5)
         # self.rect.x = start_point[0]
         # self.rect.y = start_point[1]
