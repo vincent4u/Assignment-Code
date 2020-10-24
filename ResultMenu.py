d42a9bffc6ed29dfe24b6b5b6066a30109509086
Lewis <Lewis.veryard@hotmail.co.uk>
2020-10-24 13:19:17 +0100

    score added to the win state

--- ResultMenu.py
+++ ResultMenu.py
@@ -5,6 +5,8 @@ class ResultMenu:
     def __init__(self, screen_dimension):
         self.colors=[(0,0,0), (128,128,128), (255,255,255), (255,0,0), (0,255,0)]
         self.top_middle=(screen_dimension[0]/2, screen_dimension[1]/2)
+        self.screen_dimension = screen_dimension
+        self.myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
         FONT = pygame.font.SysFont("Times New Norman", 60)
 
@@ -26,7 +28,10 @@ class ResultMenu:
     def draw_result_objects(self, screen, result, score):
         screen.fill(self.colors[2])
         if (result):
+            score = int(score * 100) / 100
+            score_surface = self.myfont.render("Score: " + str(score), False, (0, 0, 0))
             pygame.draw.rect(screen, self.buttons[0][2], self.buttons[0][1])
+            screen.blit(score_surface, ((self.screen_dimension[0] / 2) - 100, (self.screen_dimension[1] /2) - 50))
             screen.blit(self.buttons[0][0], self.buttons[0][1])
         else:
             pygame.draw.rect(screen, self.buttons[1][2], self.buttons[1][1])
