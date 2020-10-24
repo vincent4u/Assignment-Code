13fb6b3750484fa83c6d1f61c2a4a01c4a7d4612
Leon Garza <hugo.leongarza@bt.com>
2020-10-24 13:39:19 +0100

    created function to get the input values in a string row

--- DataCollection.py
+++ DataCollection.py
@@ -1,14 +1,11 @@
 
 class DataCollection:
 
-    def __int__(self):
+    def __init__(self):
         self.data_file = open("ce889_dataCollection.csv", "a")
         self.data_file.close()
 
-    def save_current_status(self, lander, surface, controller):
-        # open file
-        self.data_file = open("ce889_dataCollection.csv", "a")
-        
+    def get_input_row(self, lander, surface, controller):
         # inputs
         current_velocity = lander.velocity
         current_speed = current_velocity.length()
@@ -30,16 +27,26 @@ class DataCollection:
             turning = [0,1]
 
         # create comma separated string row
-        status_row = str(current_speed)+"," + \
+        input_row = str(current_speed)+"," + \
                     str(current_velocity.x) + "," + \
                     str(current_velocity.y) + "," + \
                     str(current_angle) + "," + \
                     str(x_target) + "," + \
                     str(y_target) + "," + \
-                    str(dist_to_surface) + "," + \
+                    str(dist_to_surface) + ","
+
+        return input_row
+
+    def save_current_status(self, lander, surface, controller):
+        # open file
+        self.data_file = open("ce889_dataCollection.csv", "a")
+        
+        input_row = self.get_input_row(lander, surface, controller)
+
+        # add output values to the string input row
+        status_row = input_row + \
                     str(thrust) + "," + \
                     str(turning[0]) + "," + str(turning[1]) + "\n"
-        # print(status_row)
         
         # save comma separated row in the file
         self.data_file.write(status_row)
